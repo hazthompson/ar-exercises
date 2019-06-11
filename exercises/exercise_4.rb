de48f1e89ed20ee05765f1cd00757ac14e41e6b6
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-11 16:37:43 -0700

    exercise 4: query all mens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
+Whistler = Store.create(name: 'Whistler', annual_revenue: 1900000 , mens_apparel: 1, womens_apparel: 0)
+Yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
+
+@mens_stores = Store.where(mens_apparel: 1)
+
+puts @mens_stores.all.count
+
+@mens_stores.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: 1).where("annual_revenue > 1000000")
+
+@womens_stores.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end
+
